8c8ea85f89bcda45a2979f657aab975d94ceb2c7
mehedi-hasan02 <mehedisani02@gmail.com>
2024-03-15 22:32:21 +0600

    add navbar and banner section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 
 import './App.css'
+import Banner from './component/banner/Banner'
+import Header from './component/header/Header'
 
 function App() {
 
@@ -7,7 +9,8 @@ function App() {
   return (
     <>
      
-      <h1 className='text-3xl text-primary'>Vite + React</h1>
+      <Header></Header>
+      <Banner></Banner>
       
     </>
   )
